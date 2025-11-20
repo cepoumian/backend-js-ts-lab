41e9cc633642f3a53e3e3bbe812d2664f4eadaff
cepoumian <cepoumian@gmail.com>
2025-11-20 11:16:14 -0600

    refactor(ts): mode user data to its own file

--- src/ts/ex02-user-union.ts
+++ src/ts/ex02-user-union.ts
@@ -1,36 +1,6 @@
-import type { Student } from "./ex01-students.ts";
-export type { Student };
-
-export interface Teacher {
-  name: string;
-  age: number;
-  subject: string;
-}
-
-export type User = Teacher | Student;
-
-export const users: User[] = [
-  {
-    name: "Luke Patterson",
-    age: 32,
-    occupation: "Internal auditor",
-  },
-  {
-    name: "Jane Doe",
-    age: 41,
-    subject: "English",
-  },
-  {
-    name: "Alexandra Morton",
-    age: 35,
-    occupation: "Conservation worker",
-  },
-  {
-    name: "Bruce Willis",
-    age: 39,
-    subject: "Biology",
-  },
-];
+import type { Student } from "../types/ex01-students.types.js";
+import type { User, Teacher } from "../types/ex02-user-union.types.js";
+export type { Student, User, Teacher };
 
 export const logUser = ({ name, age }: User) => {
   console.log(`  - ${name}, ${age}`);
